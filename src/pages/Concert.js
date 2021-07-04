049b5906e5eb638c5236f6c403d2f22c599bc7db
Hsin <hsin@impactinstitute.com>
2021-07-04 18:32:52 +0200

    update layout

--- src/pages/Concert.js
+++ src/pages/Concert.js
@@ -1,7 +1,9 @@
 function Concert() {
   return (
-    <div class="container">
-      <h1>綠盟20 破浪前行活動花絮</h1>
+    <div class="container my-5">
+      <div class="row justify-content-center align-items-center">
+        <h1>綠盟20 破浪前行活動花絮</h1>
+      </div>
     </div>
   )
 }
