eb0cd4953291a1860888cfc536a0700e005dc8ef
Hsin <hsin@impactinstitute.com>
2021-07-04 18:32:43 +0200

    Create Exhibition.js

---
+++ src/pages/Exhibition.js
@@ -0,0 +1,11 @@
+function Exhibition() {
+  return (
+    <div class="container my-5">
+      <div class="row justify-content-center align-items-center">
+        <h1>藝術與土地的匯流 綠盟20藝術公益聯展</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Exhibition
