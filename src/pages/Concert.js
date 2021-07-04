7a0248f5683e2daa3b4fa63546f7e8983cc156ac
Hsin <hsin@impactinstitute.com>
2021-07-04 20:12:36 +0200

    fix layout spacing

--- src/pages/Concert.js
+++ src/pages/Concert.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap"
 
 function Concert() {
   return (
-    <Container my="5">
+    <Container className="my-5">
       <Row class="row justify-content-center align-items-center">
         <h1>綠盟20 破浪前行活動花絮</h1>
       </Row>
--- src/pages/Exhibition.js
+++ src/pages/Exhibition.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 
 function Exhibition() {
   return (
-    <Container my="5">
+    <Container className="my-5">
       <Row class="row justify-content-center align-items-center">
         <h1>藝術與土地的匯流 綠盟20藝術公益聯展</h1>
       </Row>
