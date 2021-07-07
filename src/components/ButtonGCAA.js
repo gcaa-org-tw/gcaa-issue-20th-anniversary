84eb07bb6d1d40f5090900dd680553c53cbb638a
Hsin <hsin@impactinstitute.com>
2021-07-07 10:32:52 +0200

    create button for linking to youtube recording

--- src/components/ButtonGCAA.js
+++ src/components/ButtonGCAA.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-export default function ButtonDonation(props) {
+export default function ButtonGCAA(props) {
+  const url = props.href ?? "https://gcaa.neticrm.tw/civicrm/contribute/transact?reset=1&id=3"
+  const text = props.text ?? "支持綠盟"
   let iconLove, iconNav;
   if (props.WithLove) { iconLove = <span><i class="bi bi-suit-heart-fill"></i></span> }
   if (props.WithNav) { iconNav = <span><i class="bi bi-box-arrow-right"></i></span> }
   return (
     <a
     class="btn btn-primary"
-    href="https://gcaa.neticrm.tw/civicrm/contribute/transact?reset=1&id=3"
+    href={url}
     role="button"
     target="_blank"
     rel="noreferrer"
   >
-    { iconLove } 支持綠盟 { iconNav }
+    { iconLove } { text } { iconNav }
   </a>
   )
 }
