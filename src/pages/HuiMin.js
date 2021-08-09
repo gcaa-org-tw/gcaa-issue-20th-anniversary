d28adfdf8bf9659904bfde310e2981ade6de996b
Hsin <hsin@impactinstitute.com>
2021-08-09 13:26:19 +0200

    add contents in memory of Hui-min

--- src/pages/HuiMin.js
+++ src/pages/HuiMin.js
@@ -14,6 +14,8 @@ export default function HuiMin() {
       <Row className="justify-content-center align-items-center">
         <Col className="col-10 col-md-8 col-lg-9">
           {/* content */}
+          <p>現任綠色公民行動聯盟理事、癌症希望基金會專員、台灣公共衛生促進協會常務理事、自由撰稿人，曾任綠色公民行動聯盟專職工作者、聯合報記者。</p>
+          <p>共同參與創設「綠色公民行動聯盟」，積極投身環運，也長年擔任綠盟的理事，成為終身的環境志工。2017年即使罹癌仍積極參與公益，於綠盟的二十週年紀念音樂會上，以創會成員的身分上台致詞，回顧環境運動路上的點滴，獲得與會者的敬佩與感動，2021年6月因病離世，在此為我們的同志與夥伴，致上永遠的紀念。</p>
         </Col>
         <Col className="col-11 col-md-8 col-lg-6">
           <CardCTA />
